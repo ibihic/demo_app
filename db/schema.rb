714e30c8bf7a37275e7bcfd3b809958f54735cae
xyz <ibihic@ibihic.com>
2013-12-16 11:45:16 -0800

    Add comments model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131017025603) do
+ActiveRecord::Schema.define(version: 20131216193657) do
+
+  create_table "comments", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "microposts", force: true do |t|
     t.string   "content"
